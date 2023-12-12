82607edad0cc0384bc3903e7271c2f3f0902a83f
Priyanshu <priyanshukaushal878@gmail.com>
2023-12-12 07:34:36 +0530

    Add:Database Config and intialzation

--- CapstoneProject/Job_listing_App/server/index.js
+++ CapstoneProject/Job_listing_App/server/index.js
@@ -12,6 +12,22 @@ app.get("/", (req,res) => {
     res.send("Welcome to the API!")
 })
 
+// Health API
+app.get('/health', (req, res)=>{
+    const serverStatus = "OK"
+    const currentTime = new Date().toLocaleString()
+    const serverName = "Job Listing API"
+    const healthInfo = {
+        serverName: serverName,
+        serverStatus: serverStatus,
+        currentTime: currentTime
+    }
+    res.send(healthInfo)
+})
+
 app.listen(process.env.Port,() => {
+    mongoose.connect(process.env.MongoDB_URL)
+    .then(() => {console.log("Connected to MongoDB")})
+    .catch((err)=>{console.log('MongoDB connection error:', err)})
     console.log("Server Started at http://localhost:"+ process.env.Port)
 })
